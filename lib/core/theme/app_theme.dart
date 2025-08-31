7096e99c73af853328a3cf3fc4b1a19285ac18f2
NadilGT <rccnadildinsara@gmail.com>
2025-08-31 15:51:23 +0530

    SignIn page UI done

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,33 +4,50 @@ import '../constants/color_manager.dart';
 
 class AppTheme {
   static final lightTheme = ThemeData(
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+        color: AppColors.primaryColor,
+      ),
+    ),
     primaryColor: AppColors.primaryColor,
     scaffoldBackgroundColor: AppColors.mainWhite,
     brightness: Brightness.light,
     fontFamily: "LeagueSpartan",
     inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: Colors.transparent,
-        contentPadding: EdgeInsets.all(30),
-        hintStyle: TextStyle(
-          color: Color(0xFF383838),
-          fontWeight: FontWeight.w500
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: 0.4
-          )
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(30),
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: 0.4
-          )
-        )
+      filled: true,
+      fillColor: AppColors.textFieldBgColor,
+      contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      hintStyle: TextStyle(
+        color: AppColors.hintTextColor,
+        fontWeight: FontWeight.w500,
+        fontSize: 20,
       ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(
+          color: AppColors.textFieldBgColor,
+          width: 1.5,
+        ),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(color: Colors.red.shade400, width: 1.5),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(color: Colors.red.shade400, width: 1.5),
+      ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primaryColor,
@@ -38,12 +55,10 @@ class AppTheme {
         textStyle: TextStyle(
           fontFamily: "LeagueSpartan",
           fontSize: 24,
-          fontWeight: FontWeight.w600
+          fontWeight: FontWeight.w600,
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30)
-        )
-      )
-    )
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
   );
 }
