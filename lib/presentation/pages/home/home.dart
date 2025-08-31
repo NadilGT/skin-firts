f81eade1dc76fb006451d7fd98da62f4ebafb859
NadilGT <rccnadildinsara@gmail.com>
2025-08-31 22:45:54 +0530

    Home screen top bar done

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:skin_firts/core/constants/color_manager.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -6,8 +8,69 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 60, right: 30, left: 30),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/profile.jpg"),
+                    ),
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hi, WelcomeBack",
+                          style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          "Nadil Dinsara",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.lightBlue,
+                        borderRadius: BorderRadius.circular(30)
+                      ),
+                      padding: EdgeInsets.all(8),
+                      width: 40,
+                      height: 40,
+                      child: SvgPicture.asset("assets/images/notification.svg"),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.lightBlue,
+                        borderRadius: BorderRadius.circular(30)
+                      ),
+                      padding: EdgeInsets.all(8),
+                      width: 40,
+                      height: 40,
+                      child: SvgPicture.asset("assets/images/setting.svg"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
